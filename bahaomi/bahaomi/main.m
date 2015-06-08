4e025fbb8e5e4b3028edec558138f7cc418465de
lamto <lihao@ysteniMac.local>
2015-06-08 17:39:30 +0800

    八毫米工程初始化

---
+++ bahaomi/bahaomi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  bahaomi
+//
+//  Created by lamto on 15/6/8.
+//  Copyright (c) 2015年 yuedongxinji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
